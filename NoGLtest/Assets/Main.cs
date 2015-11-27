33f972a11a4f8d2831b539ee50df0a6c3c14f781
Kengo Nakajima <kengo.nakajima@gmail.com>
2015-11-27 13:46:35 +0900

    correct ios and pc

--- NoGLtest/Assets/Main.cs
+++ NoGLtest/Assets/Main.cs
@@ -1,10 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Main : MonoBehaviour {
 
     Texture2D m_basetex;
     Texture2D m_woodtex;
+
+    int cur_frame;
+    float show_fps_at;
+    int cur_num=0;
+    
 	// Use this for initialization
 	void Start () {
         m_basetex = Resources.Load<Texture2D>("base") as Texture2D;
@@ -15,22 +21,53 @@ public class Main : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        bool j = Input.GetButtonDown("Jump");
+        bool j = Input.GetMouseButtonDown(0);
         if(j) {
-            Debug.Log("jmpbutton");
-            Vector2 at = new Vector2( Random.Range(-1f,1f), Random.Range(-1f,1f) );
-            Sprite ns = Sprite.Create( m_woodtex,
-                                       new Rect(0,0,48,48),
-                                       at,
-                                       128f );
-            GameObject o = new GameObject();
-            o.name = "hoge";
-            o.AddComponent<SpriteRenderer>();
-            SpriteRenderer sr = o.GetComponent<SpriteRenderer>();
-            sr.sprite = ns;
-            o.transform.Translate(1,0,0);
-            //            o.transform.Scale(2,1,1);            
-            o.transform.localScale = new Vector3(2,1,1);
+            for(int i=0;i<100;i++) {
+                Vector2 at = new Vector2( Random.Range(-3f,3f), Random.Range(-3f,3f) );
+                if( Random.Range(0f,1f) < 0.5 ) {
+                    addSprite1(at);
+                } else {
+                    addSprite2(at);
+                }
+            }
+            cur_num+=100;
         }
+
+        float nt = Time.time;
+        if( show_fps_at < nt - 1 ) {
+            show_fps_at = nt;
+            Text t = GameObject.FindWithTag("FPStext").GetComponent<Text>() as Text;
+            t.text = "fps:" + cur_frame + " num:" + cur_num;
+            cur_frame = 0;
+        }
+        cur_frame ++;
+    }
+    
+    void addSprite1( Vector2 at ) {
+        Sprite ns = Sprite.Create( m_woodtex,
+                                   new Rect(0,0,48,48),
+                                   at,
+                                   128f );
+        GameObject o = new GameObject();
+        o.name = "wood";
+        o.AddComponent<SpriteRenderer>();
+        SpriteRenderer sr = o.GetComponent<SpriteRenderer>();
+        sr.sprite = ns;
+        //        o.transform.Translate(1,0,0);
+        o.transform.localScale = new Vector3(1,1,1);
+    }
+    void addSprite2( Vector2 at ) {
+        Sprite ns = Sprite.Create( m_basetex,
+                                   new Rect(0,256-8,8,8),
+                                   at,
+                                   128f );
+        GameObject o = new GameObject();
+        o.name = "base";
+        o.AddComponent<SpriteRenderer>();
+        SpriteRenderer sr = o.GetComponent<SpriteRenderer>();
+        sr.sprite = ns;
+        //        o.transform.Translate(0,-0.5f,0);
+        o.transform.localScale = new Vector3(2,2,1);        
 	}
 }
