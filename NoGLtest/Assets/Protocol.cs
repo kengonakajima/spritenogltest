4a6ab2728463249b7f8318e034ee914cc1f35484
Kengo Nakajima <kengo.nakajima@gmail.com>
2015-11-28 11:43:53 +0900

    memstream

--- NoGLtest/Assets/Protocol.cs
+++ NoGLtest/Assets/Protocol.cs
@@ -7,23 +7,39 @@ using System.Net.Sockets;
 public class Protocol : MonoBehaviour {
     TcpClient m_cli;
     NetworkStream m_stream;
-    
+    MemoryStream m_ms;
+    byte[] m_readbuf;
     void Start () {
+        m_readbuf = new byte[1024*16];
+        m_ms = new MemoryStream();
         setupClient();
+    }
+    void parseStream( MemoryStream ms ) {
+        byte[] b = ms.GetBuffer();
+        Debug.Log( "blen:" + b.Length );
     }
     private void readCallback(IAsyncResult ar ) {
         int bytes = m_stream.EndRead(ar);
-        Debug.Log("readCallback: bytes:" + bytes );
+        Debug.Log("readCallback:"+bytes);
+        try {
+            m_ms.Write( m_readbuf, 0, bytes );
+            Debug.Log( "ms:" + m_ms.Position );
+            parseStream(m_ms);
+            Debug.Log( "ms after read:" + m_ms.Position );            
+        } catch( Exception e ) {
+            Debug.Log(e);            
+        }
+        m_stream.BeginRead( m_readbuf, 0, m_readbuf.Length, new AsyncCallback(readCallback), null );
     }
     void Update () {
-        byte[] readbuf = new byte[1024];
-        IAsyncResult ar = m_stream.BeginRead( readbuf, 0, readbuf.Length, new AsyncCallback(readCallback), null );
+
     } 
     void setupClient() {
         try {
             m_cli = new TcpClient( "127.0.0.1", 23333 );
             m_stream = m_cli.GetStream();
             Debug.Log("Socket ok, stream created" );
+            m_stream.BeginRead( m_readbuf, 0, m_readbuf.Length, new AsyncCallback(readCallback), null );
         } catch (Exception e) {
             Debug.Log("Socket error: " + e);
         }
